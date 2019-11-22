e53c35e5d63915b4127262480e469b05b300ded3
gookbob <58072554+gookbob@users.noreply.github.com>
2019-11-22 19:47:57 +0900

    Add files via upload

---
+++ deezbot.py
@@ -0,0 +1,27 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(client.user.id)
+    print("ready")
+    game = discord.Game("DEEZ 서버")
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("/deezsvon"):
+        channel = client.get_channel(630432867683401824)
+        await client.get_channel(int(630432867683401824)).send("@everyone                                                                                                                                                                                                                                                                                                                                                                                        DEEZ SERVER [ ON ]                                                                                                                                                                                                                                                                                                                                                                                                                                                                   서버 주소 : deez.r-e.kr                                                                                                                                                                                                                                                                                                                                                                                                                                                               서버가 열린 후 바로 들어오시면 서버 렉의 원인이 됩니다.                                                                                                                                                                                                                                                                                                                                                                                                                                                                 조금만 기다렸다가 접속해 주세요")
+
+
+
+
+
+client.run("NjQ3MzEzNTUyMzY0Nzk3OTcy.XdeXzg.YYwcAHGUXvMOgIvJnWeB4UGPDuA")
+
+
+
