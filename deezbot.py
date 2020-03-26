19216f2bff002ad9d048323ba34decd4d2449964
gookbob <58072554+gookbob@users.noreply.github.com>
2020-03-27 08:16:57 +0900

    Update deezbot.py

--- deezbot.py
+++ deezbot.py
@@ -25,8 +25,6 @@ async def on_message(message):
         await message.channel.send("http://inven.co.kr/maple2/")
     if message.content.startswith("메콩공홈"):
         await message.channel.send("http://maplestory2.nexon.com/Main/Index")
-    if message.content.startswith("아린아"):
-        await message.channel.send("뭐해")
 
        
 
