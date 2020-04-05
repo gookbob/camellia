7dda48e706006fd1dd4de3b1bf84773a589c79c5
gookbob <58072554+gookbob@users.noreply.github.com>
2020-04-06 06:24:33 +0900

    Update deezbot.py

--- deezbot.py
+++ deezbot.py
@@ -19,9 +19,9 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith("/학폭위"):
+    if message.content.startswith("집합"):
         channel = client.get_channel(437679614370906122)
-        await client.get_channel(int(437679614370906122)).send("학교폭력대책자치위원회가 열리니 모두 참석해주시길 바랍니다.")
+        await client.get_channel(int(437679614370906122)).send("```@everyone 집합!!!```")
        
     if message.content.startswith("악마야"):
         await message.channel.send("네")
