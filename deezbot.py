a57ae4a06d02e0fa441b6b5e9488f5d9106dd45b
gookbob <58072554+gookbob@users.noreply.github.com>
2020-05-13 06:26:03 +0900

    Update deezbot.py

--- deezbot.py
+++ deezbot.py
@@ -20,7 +20,7 @@ client = discord.Client()
 async def on_ready():
     print(client.user.id)
     print("ready")
-    game = discord.Game("메1콩")
+    game = discord.Game("메콩")
     await client.change_presence(status=discord.Status.online, activity=game)
 
 
