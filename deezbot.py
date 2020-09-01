c194a2eb358de7dd7c73f27f7cf5150539ece0d1
gookbob <58072554+gookbob@users.noreply.github.com>
2020-09-01 17:48:48 +0900

    Update deezbot.py

--- deezbot.py
+++ deezbot.py
@@ -20,7 +20,7 @@ client = discord.Client()
 async def on_ready():
     print(client.user.id)
     print("ready")
-    game = discord.Game("훈련중")
+    game = discord.Game("훈련")
     await client.change_presence(status=discord.Status.online, activity=game)
 
 @client.event
@@ -369,7 +369,7 @@ async def on_message(message):
         
 @client.event
 async def on_member_join(member):
-    fmt = '{0.mention}님 {1.name}에 입학하신 것을 환영합니다. 왼쪽 메뉴 #🔪│입대신청 으로 들어가세요.'.format(member, member.guild)
+    fmt = '{0.mention}님 {1.name}에 입학하신 것을 환영합니다. 왼쪽 메뉴 #입대신청 으로 들어.'.format(member, member.guild)
     channel = member.guild.get_channel(696579283547848734)
     await channel.send(fmt)
     fmt = '@everyone 새로운 전학생 {0.mention}님이 {1.name}에 입학하셨습니다. 환영해주세요.'.format(member, member.guild)
