c9a608e3c7a5abb080ac8854071e343eeeed8b7a
gookbob <58072554+gookbob@users.noreply.github.com>
2020-10-05 03:48:39 +0900

    Update deezbot.py

--- deezbot.py
+++ deezbot.py
@@ -20,7 +20,7 @@ client = discord.Client()
 async def on_ready():
     print(client.user.id)
     print("ready")
-    game = discord.Game("훈련")
+    game = discord.Game("udt ")
     await client.change_presence(status=discord.Status.online, activity=game)
 
 @client.event
@@ -62,6 +62,15 @@ async def on_reaction_add(reaction, user):
             "```" + user.name + "님이 " + reaction.message.author.name + "에게 알겠다고 합니다. ```")
     if str(reaction.emoji) == "🔨":
         await reaction.message.channel.send("```" + user.name + "이 " + reaction.message.author.name + "의 뚝배기를 깹니다. ```")
+      
+@client.event
+async def on_member_join(member):
+    fmt = '{0.mention}님 {1.name}에 오신 것을 환영합니다. 왼쪽 메뉴 #🔪│입대신청을 눌러주세요.'.format(member, member.guild)
+    channel = member.guild.get_channel(696579283547848734)
+    await channel.send(fmt)
+    fmt = '@everyone 새로운 전학생 {0.mention}님이 {1.name}에 입학하셨습니다. 환영해주세요.'.format(member, member.guild)
+    channel = member.guild.get_channel(711174205957472307)
+    await channel.send(fmt)
 
 
 
