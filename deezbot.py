f7d54f7fd6704be2fa17f22f1914fec41493b49d
gookbob <58072554+gookbob@users.noreply.github.com>
2021-02-01 16:53:19 +0900

    Update deezbot.py

--- deezbot.py
+++ deezbot.py
@@ -66,6 +66,7 @@ async def on_reaction_add(reaction, user):
 
 
 @client.event
+async def on_member_join(member):
     fmt = '{0.mention}님 {1.name}에 오신 것을 환영합니다. 왼쪽 메뉴 #🔪│입대신청을 눌러주세요.'.format(member, member.guild)
     channel = client.get_channel(696579283547848734)
     await channel.send(fmt)
